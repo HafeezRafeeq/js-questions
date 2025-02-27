158d940d860fd0d13920365422eeb0625bea9b5d
HafeezRafeeq <hafeezrafeeq021@gmail.com>
2025-02-27 11:48:31 -0700

    2 Exercises complete

---
+++ app.js
@@ -0,0 +1,90 @@
+//chapter 1 //////////////////////////////////////////////////////////
+
+
+// 1 question solve
+// alert
+
+
+
+// 2 question solve
+// alert ("Erorr! Eenter please a Valid Password");
+
+
+// 3 question solve
+// alert ("Welcome to js lend.....  \nHappy Coding") ;
+
+
+// 4 question solve
+// alert ("Welcome to js lend.....");
+// alert ("Happy Coding.");
+
+
+// 5 question solve
+// alert ("Hello ...I can run js  through browser’s developer console")
+// console.log ("Hello ...I can run js  through browser’s developer console")
+
+
+
+//chapter 2 ///////////////////////////////////////////////////////////////
+
+// 1 question solve
+// var username = ("AbdulHafeez")
+// console.log (username)
+
+
+
+// 2 question solve
+// var myname = ("AbdulHafeez")
+// console.log (typeof"AbdulHafeez->",myname)
+
+// 3 question solve
+// var message = alert("hello world")
+// console.log ("message")
+
+
+// 4 question solve
+// var useName = alert ("Abdul Hafeez")
+// var studentAge = alert (18)
+// var message = alert ("Certified Mobile Applecation Development")
+
+// console.log ("useName")
+// console.log ("studentAge")
+// console.log ("message")
+
+
+// 5 question solve
+// var service = alert ("PIZZA \nPIZZ \nPIZ \nPI \nP")
+// console.log ("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+
+// 6 question solve
+// var email = alert ("My Email is example@example.com")
+// console.log ("My Email is " + " example@example.com" )
+
+
+// 7 question solve
+// var bookGive = alert ("A smarter way to learn JavaScript")
+
+
+// 8 question solve
+// document.write ("<br> Yah! I can write HTML content through java script")
+
+
+//chapter 3 ///////////////////////////////////////////////////////////////
+
+// 1 question solve
+// var age = alert ("I 20 Years old")
+// console.log (typeof 20)
+
+
+// 2 question solve
+// var visitTiem = alert ("You have visited this site 20 times")
+
+
+
+
+
+
+
+
+
